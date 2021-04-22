f920084759059bdef0225a5a98d629c7860d5a68
Arup Biswas <Arup.Biswas@lntinfotech.com>
2021-04-22 19:18:53 +0530

    another change

---
+++ cypress/integration/ifconditions.js
@@ -0,0 +1,16 @@
+///<reference types="Cypress"/>
+
+describe('if conditions on the wikipedia website',function(){
+
+it('apply the if condition', function()
+{
+    cy.visit('www.wikipedia.org')
+
+})
+
+
+
+
+
+
+})
