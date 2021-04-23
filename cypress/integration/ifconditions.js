77dddbae613b09181cd098fb035242a9a2b5ba7b
Arup Biswas <Arup.Biswas@lntinfotech.com>
2021-04-23 10:17:36 +0530

    new condition file update

--- cypress/integration/ifconditions.js
+++ cypress/integration/ifconditions.js
@@ -1,10 +1,28 @@
 ///<reference types="Cypress"/>
 
-describe('if conditions on the wikipedia website',function(){
-
-it('apply the if condition', function()
+describe('if conditions on the wikipedia website',function()
 {
-    cy.visit('www.wikipedia.org')
+
+it('apply the if condition when wikivoyage exist', function()
+{
+    cy.visit('https://www.wikipedia.org/',{timeout:7000})
+    cy.get('body').then(function($body)
+    {
+    if ($body.find('[data-jsl10n="wikivoyage.name"]').length>0)
+    {   
+        cy.log($body.find('[data-jsl10n="wikivoyage.name"]').length)
+        cy.get('[data-jsl10n="wikivoyage.name"]').click()
+    }
+    else
+    {
+        cy.get('[data-jsl10n="wiktionary.name"]').click()
+    } 
+        
+    })
+
+      
+    
+    
 
 })
 
